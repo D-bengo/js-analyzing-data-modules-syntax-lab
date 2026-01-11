6c634c12ea64953770987c18c7b0c534dbc0fd36
D-bengo <kichodenis31@gmail.com>
2026-01-11 11:42:39 +0300

    Done the assignment

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  }
+
+  //Merging arrays together into the users attribute of combinedObject using spread operator
+  for(let eachArray of args) {
+    combinedObject.users.push(...eachArray)
+  }
+
+  //Adding date to cobined Oject
+  combinedObject.merge_date = new Date().toString("M/d/yyyy")
+
+  //Return Combined Object
+  return combinedObject
+
+}
 
 
 module.exports = {
